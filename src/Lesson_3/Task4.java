9b051611fbc29e35bec0dcda073f0079f5650edf
“Alesia <Alesja_koval@rambler.ru>
2021-11-10 00:22:47 +0300

    Lesson_3

---
+++ src/Lesson_3/Task4.java
@@ -0,0 +1,17 @@
+package Lesson_3;
+
+import java.util.IllegalFormatCodePointException;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int mass[]=new int[] {2,2,6,4,5,7};
+        int sum=0;
+        for (int i=0;i<mass.length;i++){
+            if (mass[i]%2==0)
+            sum+=mass[i];
+        }
+        System.out.println(sum);
+    }
+    }
+
+
