a73d5eca6edba7ae013ed5f20de6abda94cc8a52
“Alesia <Alesja_koval@rambler.ru>
2021-11-10 14:39:52 +0300

    Lesson_2

--- src/Lesson_2/Task_1.java
+++ src/Lesson_2/Task_1.java
@@ -3,6 +3,6 @@ package Lesson_2;
 public class Task_1 {
     public static void main(String[] args) {
         int a=-2;
-        System.out.println((a>0)||!(a<0));
+        System.out.println(a>0);
     }
 }
